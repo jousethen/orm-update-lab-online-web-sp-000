9ea4b338fa5f653cb2d917186d84b8b58d197cbc
Jouse then <jthen93@gmail.com>
2021-03-28 18:50:41 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -23,7 +23,12 @@ class Student
   end
   
   def update
-    
+    sql = <<-SQL
+        UPDATE students
+        SET name = ?, grade = ? WHERE id = ?
+      SQL
+      
+      DB[:conn].execute(sql, self.name, self.grade, self.id)
   end
   def self.create(name:, grade:)
     student = Student.new(name, grade)
