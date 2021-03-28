518f2b2c5080210add2e0ac85fe8e08dd63bcd46
Jouse then <jthen93@gmail.com>
2021-03-28 18:40:40 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -9,17 +9,6 @@ class Student
     @grade = grade
   end
   
-  def self.create_table
-    sql = <<-SQL
-    CREATE TABLE IF NOT EXISTS students (
-      id INTEGER PRIMARY KEY,
-      name TEXT,
-      grade TEXT
-    )
-    SQL
-    DB[:conn].execute(sql)
-  end
-  
   def save
     if self.id
       self.update
@@ -31,6 +20,27 @@ class Student
       DB[:conn].execute(sql, self.name, self.grade)
       @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
     end
+  end
+  
+  def self.create(name:, grade:)
+    student = Student.new(name, grade)
+    student.save
+    student
+  end
+  
+  def self.new_from_db(row)
+    
+  end
+  
+  def self.create_table
+    sql = <<-SQL
+    CREATE TABLE IF NOT EXISTS students (
+      id INTEGER PRIMARY KEY,
+      name TEXT,
+      grade TEXT
+    )
+    SQL
+    DB[:conn].execute(sql)
   end
   
   def self.drop_table
